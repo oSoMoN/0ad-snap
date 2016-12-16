1f2904c3d4f006ded28f3ade58f1fa105752b087
Olivier Tilloy <olivier@tilloy.net>
2016-12-16 11:26:06 +0100

    Apply ubuntu patch to allow building as root/fakeroot.

--- parts/plugins/x_0adbuild.py
+++ parts/plugins/x_0adbuild.py
@@ -5,6 +5,11 @@ import snapcraft
 class X0ADBuildPlugin(snapcraft.BasePlugin):
 
     def build(self):
+        patches_dir = os.path.join(self.project.parts_dir, 'patches')
+        patches = ['allow-build-with-root.patch']
+        for patch in patches:
+            with open(os.path.join(patches_dir, patch), 'rb', 0) as pfile:
+                self.run(['patch', '-d', self.sourcedir, '-p1'], stdin=pfile)
         parallel_arg = '-j{}'.format(self.parallel_build_count)
         self.run(['build/workspaces/update-workspaces.sh',
                   '--with-system-nvtt',
